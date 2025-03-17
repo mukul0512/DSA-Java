d03dc7844096047aebcd0975a5cee4933eb51de7
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-17 11:37:38 +0530

    About split() and trim() function

--- Leetcode/ReverseWordsInString.java
+++ Leetcode/ReverseWordsInString.java
@@ -1,16 +1,16 @@
 public class ReverseWordsInString {
     public static String reverseWordsInString(String s) {
-        String words[] = s.split(" +");
+        String words[] = s.split(" +"); // split(" +") function is used for separation of words.
         StringBuilder sb = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
             sb.append(words[i]);
             sb.append(" ");
         }
-        return sb.toString().trim();
+        return sb.toString().trim(); // trim() function is used for remove leading and trailing spaces(extra spaces).
     }
 
     public static void main(String[] args) {
-        String s = "the sky is blue";
+        String s = " hello world ";
         System.out.println(reverseWordsInString(s));
     }
 }
