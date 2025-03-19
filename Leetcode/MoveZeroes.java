dca7de373843a02d971ef78c1a4af847c0b0fb8a
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-19 23:05:11 +0530

    Solved MoveZeroes Prob.

---
+++ Leetcode/MoveZeroes.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void moveZeroes(int nums[]) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[count++] = nums[i];
+            }
+        }
+        while (count < nums.length) {
+            nums[count++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 0, 1, 0, 3, 12 };
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
