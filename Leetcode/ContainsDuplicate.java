79a9c574bef3642dfad64cc7afb0e4bea4187ff4
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-19 21:32:41 +0530

    Solved ContainsDuplicate Prob.

---
+++ Leetcode/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int nums[]) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i])) {
+                return true;
+            } else {
+                hm.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 1 };
+        System.out.println(containsDuplicate(nums));
+    }
+}
