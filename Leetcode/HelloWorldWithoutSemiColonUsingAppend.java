e1b35fbea269d2e586adf4961342d11f109b0dbc
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-21 11:23:14 +0530

    Print Hello World Without Semicolon Using append()

---
+++ Leetcode/HelloWorldWithoutSemiColonUsingAppend.java
@@ -0,0 +1,12 @@
+public class HelloWorldWithoutSemiColonUsingAppend {
+    public static void helloWorldWithoutSemiColonUsingAppend() {
+        if (System.out.append("Hello World") == null) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        helloWorldWithoutSemiColonUsingAppend();
+    }
+
+}
