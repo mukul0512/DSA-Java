335c8a5b6e1d667f175fb11279b7497f0c5aea69
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-21 11:44:35 +0530

    Print Hello World Without semi-colon usin equals()

---
+++ Leetcode/HelloWorldWithoutSemiColonUsingEquals.java
@@ -0,0 +1,12 @@
+public class HelloWorldWithoutSemiColonUsingEquals {
+    public static void helloWorldWithoutSemiColonUsingEquals() {
+        if (System.out.append("Hello World").equals(null)) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        helloWorldWithoutSemiColonUsingEquals();
+    }
+
+}
