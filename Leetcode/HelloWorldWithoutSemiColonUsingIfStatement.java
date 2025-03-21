17b068670a3f46748612cdfed5dddb50f1f62d0c
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-21 11:07:06 +0530

    Print Hello World Without Semi-Colon using if{}

---
+++ Leetcode/HelloWorldWithoutSemiColonUsingIfStatement.java
@@ -0,0 +1,12 @@
+public class HelloWorldWithoutSemiColonUsingIfStatement {
+    public static void helloWorldWithoutSemiColonUsingIfStatement() {
+        if (System.out.printf("Hello World") == null) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        helloWorldWithoutSemiColonUsingIfStatement();
+    }
+
+}
