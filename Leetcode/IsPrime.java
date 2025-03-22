55ce0b610abc2cde400e6756dbbfe1031fa23244
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-22 13:03:13 +0530

    updated input taken from user

--- Leetcode/IsPrime.java
+++ Leetcode/IsPrime.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class IsPrime {
     public static boolean isPrime(int num) {
         if (num < 2)
             return false; // 0 and 1 are not prime numbers
 
-        for (int i = 2; i * i <= num; i++) { // Optimized loop till sqrt(num)
+        for (int i = 2; i * i <= num; i++) { // Optimized loop till sqrt(num). i * i <= num is a clever way of writing i
+                                             // <= Math.sqrt(num) without using the Math class.
             if (num % i == 0) {
                 return false; // If num is divisible by i, it's not prime
             }
@@ -12,7 +15,10 @@ public class IsPrime {
     }
 
     public static void main(String[] args) {
-        int num = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to check if it's prime: ");
+        int num = sc.nextInt();
+        sc.close();
         System.out.println(num + " is " + (isPrime(num) ? "a prime number" : "not a prime number"));
     }
 }
