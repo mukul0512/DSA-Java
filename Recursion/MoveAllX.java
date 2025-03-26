f685a4ea06bd8f02e08f9e11e87ff4e7a2f5ad2c
Mukul Karnwal <karnwal.mukul@gmail.com>
2025-03-26 11:51:24 +0530

    Minor change

--- Recursion/MoveAllX.java
+++ Recursion/MoveAllX.java
@@ -2,25 +2,25 @@ package Recursion;
 
 public class MoveAllX {
     public static void moveAllX(String str, int idx, int count, String newString) {
-        if(idx == str.length()) {
-            for(int i = 0; i < count; i++) {
+        if (idx == str.length()) {
+            for (int i = 0; i < count; i++) {
                 newString += 'x';
             }
             System.out.println(newString);
             return;
         }
         char currChar = str.charAt(idx);
-        if(currChar == 'x') {
+        if (currChar == 'x') {
             count++;
             moveAllX(str, idx + 1, count, newString);
-        }
-        else {
+        } else {
             newString += currChar;
             moveAllX(str, idx + 1, count, newString);
         }
     }
+
     public static void main(String args[]) {
         String str = "axbcxxd";
-        moveAllX(str, 0, 0, " ");
+        moveAllX(str, 0, 0, "");
     }
 }
